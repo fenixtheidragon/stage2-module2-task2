6d6b2eb827e5c354ad8cc47152e8871087935453
fenixtheidragon <fenixtheidragon@gmail.com>
2024-05-16 15:02:50 +0300

    initial

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -1,5 +1,16 @@
 package com.example.listener;
 
-public class ContextListener {
-    //write your code here!
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.time.LocalDateTime;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+
+  @Override
+  public void contextInitialized(ServletContextEvent event) {
+    LocalDateTime localDateTime = LocalDateTime.now();
+    event.getServletContext().setAttribute("servletTimeInit", localDateTime);
+  }
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,52 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import com.example.Users;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+  private final static String helloPage = "/user/hello.jsp";
+  private final static String loginPage = "/login.jsp";
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    HttpSession session = request.getSession();
+    String result;
+    if (session.getAttribute("user") == null) {
+      result = loginPage;
+    } else {
+      result = helloPage;
+    }
+    sendRedirect(response, result);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) {
+    String user = request.getParameter("login");
+    String password = request.getParameter("password");
+    String result;
+    if (Users.getInstance().getUsers().contains(user) && !password.isEmpty()) {
+      HttpSession session = request.getSession();
+      session.setAttribute("user", user);
+      result = helloPage;
+    } else {
+      result = loginPage;
+    }
+    sendRedirect(response, result);
+  }
+
+  private void sendRedirect(HttpServletResponse response, String result) {
+    try {
+      response.sendRedirect(result);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
